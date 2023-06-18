3b7869fa3602ff0ce36a985dea8f36bb8ac6f617
Ivan Koshevoy <ivan.koshevoyyy@gmail.com>
2023-06-18 19:51:08 +0100

    removed duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={"10px"} overflow={"hidden"} width="300px">
+    <Card>
       <Image src={getCroppedUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
