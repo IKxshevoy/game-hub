7f28ca28d7185d557c41cd24bff8ec5abde0c1f5
Ivan Koshevoy <ivan.koshevoyyy@gmail.com>
2023-06-25 01:20:11 +0100

    refactored api-client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "../services/api-client";
-import apiClient from "../services/api-client";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -11,10 +12,7 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Genre>>("/genres")
-        .then((response) => response.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
   });
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
-  }
-  
+  count: number;
+  results: T[];
+}
 
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '892d2a1d73064259ad35bb1258547fc5'
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "892d2a1d73064259ad35bb1258547fc5",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((response) => response.data);
+  };
+}
+
+export default APIClient;
