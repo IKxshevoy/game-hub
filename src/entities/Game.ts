990628d5f31ed4fd4f25dc34508d15e7cbe59273
Ivan Koshevoy <ivan.koshevoyyy@gmail.com>
2023-07-01 22:39:02 +0100

    added game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform; }[];
   metacritic: number;
   slug: string;
   description_raw: string;
+  publishers: Publisher[];
 }
